eea6ce74a8b59e363d2fe0b7e1fdd55598776f4b
DarshitJain04 <jain.38@iitj.ac.in>
2020-08-31 19:28:11 +0530

    changed name for alarm from link to start_time

--- background.js
+++ background.js
@@ -11,20 +11,19 @@ chrome.storage.sync.get(["Data"], function (res) {
     for (let i = 0; i < data.length; i++) {
         const link = data[i]["link"];
         if (data[i]["start_time"] >= Date.now()) {
-            chrome.alarms.create(link, {
+            chrome.alarms.create(data[i]["start_time"].toString(), {
                 when : data[i]["start_time"]
             });
             console.log(`alarm ${ i } created`);
+            chrome.alarms.onAlarm.addListener( function (alarm) {
+                window.open(link, "_blank");
+                chrome.alarms.clear(alarm["name"]);
+            });
         };
     };
 
     chrome.alarms.getAll(function (alarms) {
         console.log(`Alarms : ${ JSON.stringify(alarms) }`)
-    });
-
-    chrome.alarms.onAlarm.addListener( function (alarm) {
-        window.open(alarm["name"], "_blank");
-        chrome.alarms.clear(alarm["name"]);
     });
 
 });
