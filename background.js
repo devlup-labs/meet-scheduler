2dcbadc5bb5a06e80a8cc0108000af1368e543ba
DarshitJain04 <jain.38@iitj.ac.in>
2020-09-05 22:16:13 +0530

    added function to update background script

--- background.js
+++ background.js
@@ -1,29 +1,40 @@
-chrome.alarms.clearAll(function () {
-    console.log("Clearing all alarms");
-});
+// Added listner to capture message
+chrome.runtime.onMessage.addListener(
+    function() {
 
-chrome.storage.sync.get(["Data"], function (res) {
+        console.log("Message received from popup js");
 
-    var data = res.Data
-    console.log(`If condition data : ${ JSON.stringify(data) }`)
+        // Clear all alarms when new data is added
+        chrome.alarms.clearAll(function () {
+            console.log("Clearing all alarms");
+        });
 
-    for (let i = 0; i < data.length; i++) {
-        if (data[i]["start_time"] >= Date.now()) {
-            chrome.alarms.create(data[i]["id"], {
-                when : data[i]["start_time"]
+
+        // Fetching data and creating all the alarms again
+        chrome.storage.sync.get(["Data"], function (res) {
+
+            var data = res.Data;
+            console.log(`Data stored : ${ JSON.stringify(data) }`);
+
+            for (let i = 0; i < data.length; i++) {
+                if (data[i]["start_time"] >= Date.now()) {
+                    chrome.alarms.create(data[i]["id"], {
+                        when : data[i]["start_time"]
+                    });
+                    console.log(`Alarm ${ i } created`);
+                };
+            };
+
+            chrome.alarms.onAlarm.addListener( function (alarm) {
+                var link = data.find(a => a.id === alarm["name"])["link"]
+                console.log("Current link :" + link);
+                window.open(link, "_blank");
+                chrome.alarms.clear(alarm["name"]);
             });
-            console.log(`alarm ${ i } created`);
-        };
-    };
 
-    chrome.alarms.onAlarm.addListener( function (alarm) {
-        var link = data.find(a => a.id === alarm["name"])["link"]
-        window.open(link, "_blank");
-        chrome.alarms.clear(alarm["name"]);
-    });
+            chrome.alarms.getAll(function (alarms) {
+                console.log(`Alarms : ${ JSON.stringify(alarms) }`)
+            });
 
-    chrome.alarms.getAll(function (alarms) {
-        console.log(`Alarms : ${ JSON.stringify(alarms) }`)
-    });
-
+        });
 });
--- popup.js
+++ popup.js
@@ -18,7 +18,7 @@ chrome.storage.sync.get(["Data"], function (res) {
     }
 });
 
-class UserData {
+class AlarmData {
     constructor(link, description, startdata) {
         this.generateID = function () {
             var dt = new Date().getTime();
@@ -43,25 +43,26 @@ function OpenLink() {
     var description = document.getElementById("description").value;
     var startdata = document.getElementById("start_time").value;
 
-    var alarm_data = new UserData(link, description, startdata)
+    var alarm_data = new AlarmData(link, description, startdata)
 
     data.push(alarm_data)
 
+    // Display all the data on the console
     console.log(data)
 
+    // Add new data
     chrome.storage.sync.set({"Data" : data}, function () {
         console.log('Data is added');
     });
 
+    // Send message to background script when new data is stored so as to update it
+    chrome.runtime.sendMessage({greeting: "hello"}, function() {
+        console.log("Message sent");
+    });
+
+    // Update the current data variable
     chrome.storage.sync.get(["Data"], function (res) {
         data = res.Data
-        for (let i = 0; i < data.length; i++) {
-            var node = document.createElement("LI");
-            var list_data = `Link : ${data[i]["link"]}\nDescription : ${data[i]["description"]}\nTime : ${data[i]["startdata"]}`;
-            var textnode = document.createTextNode(list_data);
-            node.appendChild(textnode);
-            document.getElementById("data_list").appendChild(node);
-        }
     });  
 
     document.getElementById("link").value = "";
