f06143c7a90f8cd40a18b5613350576f992cb683
Hiren Chalodiya <chalodiya.1@iitj.ac.in>
2020-09-06 20:04:01 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,53 +1,99 @@
-var button  = document.getElementById("submit_button");
-button.addEventListener('click', OpenLink)
+var button = document.getElementById("submit_button");
+button.addEventListener("click", button_click);
+update_list();
 
-var data = []
-chrome.storage.sync.get(["Data"], function (res) {
-    if (res.Data == undefined) {
-        document.getElementById("data_list").innerHTML = "No data added";
-    }
-    if (res.Data.length > 0) {
-        data = res.Data
-        for (let i = 0; i < data.length; i++) {
-            var node = document.createElement("LI");
-            var list_data = `Link : ${data[i]["link"]}\nDescription : ${data[i]["description"]}\nTime : ${data[i]["startdata"]}`;
-            var textnode = document.createTextNode(list_data);
-            node.appendChild(textnode);
-            document.getElementById("data_list").appendChild(node);
-        }
-    }
-});
-
-class UserData {
-    constructor(link, description, startdata) {
-        this.link  = link
-        this.description = description
-        this.startdata = new Date(startdata).toString()
-        this.start_time = new Date(startdata).getTime()
-    }
+function sendMessage(type, key) {
+  let message = {};
+  message["type"] = type;
+  message["key"] = key;
+  chrome.runtime.sendMessage(message, () => {});
 }
 
-function OpenLink() {
+async function setDataIntoStorage(key, value) {
+  const dict = {};
+  dict[key] = value;
+  return await new Promise((resolve) => {
+    chrome.storage.sync.set(dict, resolve);
+  });
+}
 
-    var link = document.getElementById("link").value;
-    var description = document.getElementById("description").value;
-    var startdata = document.getElementById("start_time").value;
+async function getAllDataFromStorage() {
+  return await new Promise((resolve) => {
+    chrome.storage.sync.get(null, (result) => resolve(result || {}));
+  });
+}
 
-    var alarm_data = new UserData(link, description, startdata)
+async function removeDataFromStorage(key) {
+  return await new Promise((resolve) => {
+    chrome.storage.sync.remove(key, resolve);
+  });
+}
 
-    data.push(alarm_data)
+async function update_list() {
+  let data = await getAllDataFromStorage();
+  document.getElementById("data_list").innerHTML = "";
+  if (data.length != 0) {
+    for (var key in data) {
+      var node = document.createElement("LI");
 
-    console.log(data)
+      var list_data = `Link : ${data[key].link}\nDescription : ${data[key].desc}\nTime : ${data[key].startdata}`;
+      var textnode = document.createTextNode(list_data);
+      node.appendChild(textnode);
 
-    chrome.storage.sync.set({"Data" : data}, function () {
-        console.log('Data is added');
-    });
+      var button = document.createElement("button");
+      button.innerHTML = "X";
+      button.addEventListener("click", async () => {
+        await removeDataFromStorage(key);
+        update_list();
+        sendMessage("REMOVE_ALARM", key);
+      });
 
-    chrome.storage.sync.get(["Data"], function (res) {
-        data = res.Data
-    });
+      node.appendChild(button);
+      document.getElementById("data_list").appendChild(node);
+    }
+  } else {
+    document.getElementById("data_list").innerHTML = "No data added";
+  }
+}
 
-    document.getElementById("link").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("start_time").value = "";
+class Alarm {
+  constructor(link, description, startdata) {
+    this.generateID = function () {
+      var dt = new Date().getTime();
+      var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        function (c) {
+          var r = (dt + Math.random() * 16) % 16 | 0;
+          dt = Math.floor(dt / 16);
+          return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+        }
+      );
+      return uuid;
+    };
+    this.id = this.generateID();
+    this.link = link;
+    this.desc = description;
+    this.startdata = new Date(startdata).toString();
+    this.time = new Date(startdata).getTime();
+  }
+}
+
+function button_click() {
+  var link = document.getElementById("link").value;
+  var description = document.getElementById("description").value;
+  var startdata = document.getElementById("start_time").value;
+
+  // check to validate data
+  // TODO
+
+  // create alarm
+  var alarm_data = new Alarm(link, description, startdata);
+  console.log(alarm_data);
+  setDataIntoStorage(alarm_data.id, alarm_data);
+  update_list();
+  sendMessage("ADD_ALARM", alarm_data.id);
+
+  document.getElementById("link").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("start_time").value = "";
 }
