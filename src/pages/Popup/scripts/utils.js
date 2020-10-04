47e25cee146031b0e51ebb4bd19a8782415eec14
DarshitJain04 <jain.38@iitj.ac.in>
2020-10-04 17:19:10 +0530

    connected autojoin feature to meet.js

--- src/pages/Popup/scripts/utils.js
+++ src/pages/Popup/scripts/utils.js
@@ -19,27 +19,7 @@ function createTab(link, authuser, autojoin) {
             console.log('URL matched the google meet regex');
             if (autojoin) {
               console.log('Intiating autojoin');
-              chrome.tabs.executeScript(tab.id, {
-                code: `
-                  setInterval(function() {
-                  try{
-                    let micButton = document.getElementsByClassName("U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb")[0];
-                    micButton.click();
-                    console.log("Clicked mic button");
-                  } catch (err) { console.log(err); }
-                  try{
-                    let camButton = document.getElementsByClassName("U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb")[0];
-                    camButton.click();
-                    console.log("Clicked cam button");
-                  } catch (err) { console.log(err); }
-                  try{
-                    let joinButton = document.getElementsByClassName("uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt")[0];
-                    joinButton.click();
-                    console.log("Clicked join button");
-                  } catch (err) {console.log(err);}
-                  }, 8000);
-                  `,
-              });
+              chrome.tabs.executeScript(tab.id, { file: 'meet.bundle.js' });
             }
             console.log(`Status : ${info.status} and ID : ${tab.id}`);
           } else if (link.match(zoomregexExp)) {
