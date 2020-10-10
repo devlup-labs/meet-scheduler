01302e8856d2383aa779013f85fbbdaa9bd1897c
shantanu bakshi <shantanu.1@iitj.ac.in>
2020-10-10 13:54:03 +0530

    Test2 :24 hrs -time changes + appearance

--- src/pages/Popup/components/timer.jsx
+++ src/pages/Popup/components/timer.jsx
@@ -1,72 +1,5 @@
 import React, { Component } from "react";
-import './stylingtimer.css';
-
-// function method() {
-//     return 1602239900000;
-
-// }
-
-// function coolio(date) {
-//     var myDate = new Date(date);
-//     var offset = myDate.getTimezoneOffset() * 60 * 1000;
-
-//     var withOffset = myDate.getTime();
-//     var withoutOffset = withOffset - offset;
-//     console.log(withoutOffset - 19800000);
-//     return (withoutOffset - 19800000)
-// }
-
-// function extract() {
-//     timeMethod()
-//         .then(function (response) {
-//             console.log(response.data);
-//             console.log("suxy");
-//             return response;
-//         })
-
-// }
-
-// let res = extract();
-// console.log();
-
-// async function timeMethod() {
-//     let data = await getAllDataFromStorage();
-//     var alarms = await new Promise((resolve) => chrome.alarms.getAll(resolve));
-//     alarms.sort(function (a, b) {
-//         return a.scheduledTime - b.scheduledTime;
-//     });
-//     return alarms;
-
-// var setalarms = [];
-
-// for (let i = 0; i < alarms.length; i++) {
-//     var time = new Date();
-//     time.setTime(alarms[i].scheduledTime);
-//     time = time.toLocaleString();
-//     setalarms.push({
-//         time: time,
-//         id: i,
-//         name: alarms[i].name,
-//         data: data[alarms[i].name].course,
-//         status: data[alarms[i].name].status,
-//         custom: data[alarms[i].name].course.type === 'custom',
-//     });
-// }
-
-
-// var v = {};
-// timeMethod().then(function (val) {
-
-// })
-// console.log(v);
-// function extract(timeMethod) {
-//     timeMethod().then(function (result) {
-//         return result;
-//     });
-// }
-
-// var v = extract(timeMethod);
-// console.log(v);
+import './timerstyle2.css';
 
 export default class Timer extends Component {
     today = new Date();
@@ -103,9 +36,11 @@ export default class Timer extends Component {
                     return a.scheduledTime - b.scheduledTime;
                 });
                 var time = alarms[0].scheduledTime;
+
                 this.setState(() => ({
                     remTime: time - this.today
                 }))
+                console.log("ja");
                 clearInterval(this.myInterval);
             }
         }, 1000)
@@ -119,38 +54,71 @@ export default class Timer extends Component {
     render() {
 
         const { remTime } = this.state;
-        var hours = Math.floor(remTime / 3600000);
-        var minutes = Math.floor(remTime / 60000);
+        var days = 0
+        var hours = 0
+        var minutes = 0
+        var seconds = (remTime / 1000).toFixed(0);
+        if (seconds >= 60) {
+            minutes = Math.floor(seconds / 60);
+            seconds = seconds % 60
+        }
         if (minutes >= 60) {
-            hours += Math.floor(minutes / 60)
+            hours = Math.floor(minutes / 60)
             minutes = minutes % 60
         }
-        var seconds = ((remTime % 60000) / 1000).toFixed(0);
+        if (hours >= 24) {
+            days = Math.floor(hours / 24);
+            hours = hours % 24
+        }
+
         // return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
         var timeString = ""
-        if (hours >= 10) {
-            timeString = "chill Abhi time Hai 😎"
-        } else {
-            if (minutes < 10) {
-                if (seconds < 10) {
-                    timeString = "Next class in.. 0" + hours + " h: 0" + minutes + " m: 0" + seconds + " s"
+        if (days > 0) {
+            if (hours < 10) {
+                if (minutes < 10) {
+                    if (seconds < 10) {
+                        timeString = `Next class in..\n ${days} d: 0${hours} h: 0${minutes} m: 0${seconds} s`
+                    }
+                    else {
+                        timeString = `Next class in..\n ${days} d: 0${hours} h: 0${minutes} m: ${seconds} s`
+                    }
                 }
                 else {
-                    timeString = "Next class in.. 0" + hours + " h: 0" + minutes + " m: " + seconds + " s"
+                    timeString = `Next class in..\n ${days} d: 0${hours} h: ${minutes} m: ${seconds} s`
                 }
+
             }
             else {
-                timeString = "Next class in.. 0" + hours + " h: " + minutes + " m: " + seconds + " s"
+                timeString = `Next class in..\n ${days} d: ${hours} h: ${minutes} m: ${seconds} s`
+            }
+        } else {
+            if (hours < 10) {
+                if (minutes < 10) {
+                    if (seconds < 10) {
+                        timeString = `Next class in..\n 0${hours} h: 0${minutes} m: 0${seconds} s`
+                    }
+                    else {
+                        timeString = `Next class in..\n 0${hours} h: 0${minutes} m: ${seconds} s`
+                    }
+                }
+                else {
+                    timeString = `Next class in..\n 0${hours} h: ${minutes} m: ${seconds} s`
+                }
+
+            }
+            else {
+                timeString = `Next class in..\n ${hours} h: ${minutes} m: ${seconds} s`
             }
         }
         return (
             <div className="timer">
-
-                <h1 style={{
-                    textAlign: "center",
-                    fontSize: "21px"
-                }}>{timeString}</h1>
-
+                { days === 0 ?
+                    <h1 style={{
+                        textAlign: "center",
+                        fontSize: "21px",
+                        paddingTop: "6px"
+                    }}>{timeString}</h1> : ""
+                }
             </div>
         )
     }
