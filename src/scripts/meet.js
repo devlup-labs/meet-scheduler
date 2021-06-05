724342cf3565b3a97f03fdaf1cf673adcf863657
DarshitJain04 <jain.38@iitj.ac.in>
2021-06-05 11:51:37 +0530

    changed auto join script stopping criterion

--- src/scripts/meet.js
+++ src/scripts/meet.js
@@ -1,48 +1,55 @@
-var x = 1;
-setInterval(function () {
-  if(x > 30){
-    console.log('Stopped');
-    clearInterval(this);
-    
-  }
-  else{
-    x++;
-  }
-  try {
-    let micButton = document.getElementsByClassName(
-      'U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb'
-    )[0];
-    micButton.click();
-    console.log('Clicked mic button');
-  } catch (err) {
-    console.log(err);
-  }
-  try {
-    let camButton = document.getElementsByClassName(
-      'U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb'
-    )[0];
-    camButton.click();
-    console.log('Clicked cam button');
-  } catch (err) {
-    console.log(err);
-  }
-  try {
-    let joinButton = document.getElementsByClassName(
-      'uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt'
-    )[0];
-    joinButton.click();
-    console.log('Clicked join button');
-  } catch (err) {
-    console.log(err);
-  }
-  try {
-    let join = document.getElementsByClassName(
-      'U26fgb O0WRkf oG5Srb HQ8yf C0oVfc kHssdc HvOprf M9Bg4d'
-    )[0];
-    join.click();
-    console.log('Clicked Join button');
-  } catch (err) {
-    console.log(err);
-  }
-}, 8000);
-console.log('Joined the meeting!');
+const autoJoin = () => {
+  var initiateAutoJoin = setInterval(() => {
+    try {
+      var micButton = document.getElementsByClassName(
+        'U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb'
+      )[0];
+      if (typeof micButton != 'undefined' && micButton != null) {
+        micButton.click();
+        console.log('Clicked mic button');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    try {
+      var camButton = document.getElementsByClassName(
+        'U26fgb JRY2Pb mUbCce kpROve uJNmj HNeRed QmxbVb'
+      )[0];
+      if (typeof camButton != 'undefined' && camButton != null) {
+        camButton.click();
+        console.log('Clicked cam button');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    try {
+      var joinButton = document.getElementsByClassName(
+        'uArJ5e UQuaGc Y5sE8d uyXBBb xKiqt'
+      )[0];
+      if (typeof joinButton != 'undefined' && joinButton != null) {
+        joinButton.click();
+        console.log('Clicked join button');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    try {
+      var joinRecording = document.getElementsByClassName(
+        'U26fgb O0WRkf oG5Srb HQ8yf C0oVfc kHssdc HvOprf M9Bg4d'
+      )[0];
+      if (typeof joinRecording != 'undefined' && joinRecording != null) {
+        joinRecording.click();
+        console.log('Clicked join now button when recording');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    if (!micButton && !camButton && !joinButton && !joinRecording) {
+      console.log('Stopping Auto Join execution');
+      clearInterval(initiateAutoJoin);
+    }
+  }, 10000);
+  console.log('Joined the meeting!');
+};
+
+autoJoin();
